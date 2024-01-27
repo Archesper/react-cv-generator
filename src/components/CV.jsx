c66b1ceb2d14ca2c9401d054dff10fca3328e230
Arch <wbhaoui@gmail.com>
2024-01-27 16:26:52 +0100

    Add resume header styling

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,3 +1,7 @@
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
 export default function CV({ data }) {
   const { fullName, phoneNumber, email, address, education, experience } = data;
   return (
@@ -5,33 +9,47 @@ export default function CV({ data }) {
       <div className="personal-info">
         <h1 className="resume-name">{fullName}</h1>
         <div className="contact-info">
-          <span className="phone-number">{phoneNumber}</span>
-          <span className="email">{email}</span>
-          <span className="address">{address}</span>
+          {phoneNumber && (
+            <div className="contact-wrapper">
+              <LocalPhoneIcon></LocalPhoneIcon>
+              <span className="phone-number"> {phoneNumber}</span>
+            </div>
+          )}
+          {email && (
+            <div className="contact-wrapper">
+              <EmailIcon></EmailIcon>
+              <span className="email"> {email}</span>
+            </div>
+          )}
+          {address && (
+            <div className="contact-wrapper">
+              <LocationOnIcon></LocationOnIcon>
+              <span className="address"> {address}</span>
+            </div>
+          )}
         </div>
-        <h3>Education</h3>
-        {education.map((ed) => {
-          return (
-            ed.visible && (
-              <div key={ed.id}>
-                {ed.school} {ed.degree} {ed.startDate} {ed.endDate}{" "}
-                {ed.location}
-              </div>
-            )
-          );
-        })}
-        <h3>Experience</h3>
-        {experience.map((exp) => {
-          return (
-            exp.visible && (
-              <div key={exp.id}>
-                {exp.companyName} {exp.positionTitle} {exp.startDate}{" "}
-                {exp.endDate} {exp.location} {exp.description}
-              </div>
-            )
-          );
-        })}
       </div>
+      <h3>Education</h3>
+      {education.map((ed) => {
+        return (
+          ed.visible && (
+            <div key={ed.id}>
+              {ed.school} {ed.degree} {ed.startDate} {ed.endDate} {ed.location}
+            </div>
+          )
+        );
+      })}
+      <h3>Experience</h3>
+      {experience.map((exp) => {
+        return (
+          exp.visible && (
+            <div key={exp.id}>
+              {exp.companyName} {exp.positionTitle} {exp.startDate}{" "}
+              {exp.endDate} {exp.location} {exp.description}
+            </div>
+          )
+        );
+      })}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import 'reset-css'
 import './styles/main.css'
+import './styles/resume.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
