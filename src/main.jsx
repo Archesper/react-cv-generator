301c6bf53641597ebbb0747e8b31aa7af8e5bfd9
Arch <wbhaoui@gmail.com>
2024-01-30 13:18:18 +0100

    Split stylesheet into several files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import 'reset-css'
 import './styles/main.css'
+import './styles/inputs.css'
+import './styles/buttons.css'
 import './styles/resume.css'
+import './styles/print.css'
+import './styles/media.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
